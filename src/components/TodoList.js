5fd23e06f490e08d2e4af977a476d5eb30a19eb2
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-09-16 13:37:40 -0400

    Finished with html layout of each component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,9 +2,11 @@ import React from 'react'
 import TodoItem from './TodoItem'
 function TodoList() {
   return (
-    <div>
+    <ul class="list-group my-5">
+    <h3 class="text-capitalize text-center">todo list</h3>
     <TodoItem />
-    </div>
+    <button type='button' className="btn btn-danger btn-block text-capitalize mt-5">clear list</button>
+    </ul>
   )
 }
 
